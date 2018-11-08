644ff549526973f9af6ef69eb8f1f1f8098f9100
aalap <apatel@e-cobalt.com>
2018-11-08 17:07:19 -0500

    coordinator layout and code refactoring.

--- app/src/main/java/com/example/aalap/news/ui/activities/CategoryTabActivity.kt
+++ app/src/main/java/com/example/aalap/news/ui/activities/CategoryTabActivity.kt
@@ -135,16 +135,13 @@ class CategoryTabActivity : BaseActivity(), MainView {
             super.onDraw(canvas, parent, state)
             val left = parent.paddingLeft
             val right = parent.width - parent.paddingRight
-
             val childCount = parent.childCount
+
             for (i in 0 until childCount) {
                 val child = parent.getChildAt(i)
-
                 val params = child.layoutParams
-
                 val top = child.bottom + params.height
                 val bottom = top + (divider?.intrinsicHeight ?: 0)
-
                 divider?.setBounds(left, top, right, bottom)
                 divider?.draw(canvas)
             }
